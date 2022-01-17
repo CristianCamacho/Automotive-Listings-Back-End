cea8fe2105db3db3b671b502688e3a052a21dfc5
Cristian Camacho <Cristian_Camacho@aol.com>
2022-01-16 22:06:27 -0500

    Added cors. Hit back-end from front-end.

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, json, request, jsonify
+from flask import Flask, json, request, jsonify, after_this_request
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
+
+CORS(app, resources={r"/*": {"origins": os.environ.get('FRONT_END')}}, supports_credentials=True)
 
 @app.route('/')
 def ping():
