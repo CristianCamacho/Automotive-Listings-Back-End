da3cc5e90f64a943e341c34abefb997a9ac7dc99
Cristian Camacho <Cristian_Camacho@aol.com>
2022-01-20 15:03:51 -0500

    Finished get_years route.

--- resources/auto_info.py
+++ resources/auto_info.py
@@ -4,13 +4,21 @@ from resources.cache import cache
 from concurrent.futures import as_completed
 from requests_futures.sessions import FuturesSession
 from datetime import datetime
-
+import xmltodict 
 auto_info = Blueprint('auto_info', 'auto_info')
 
+session = FuturesSession()
+
 @auto_info.route('/get_years', methods=['GET'])
-@cache.cached(timeout=70)
+@cache.cached(timeout=86400)
 def get_years():
-    now = datetime.now()
+    r = requests.get('https://www.fueleconomy.gov/ws/rest/vehicle/menu/year')
+    dict_from_xml_years = xmltodict.parse(r.content)
+
+    list_of_years = []
+    for item in dict_from_xml_years['menuItems']['menuItem']:
+        list_of_years.append(item['value'])
+
     return jsonify(
-        message=now
-    )
+        years=list_of_years
+    ), 200
